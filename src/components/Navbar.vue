b393f1dfcddb47c1de0acd2671b7708389937040
deficts <diegoar7@live.com>
2019-12-11 17:53:42 -0600

    creando navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+<nav>
+  <v-app-bar app class="blue-grey--text">
+    <v-toolbar-title>
+      <router-link to="/">
+        <span class="font-weight-light">Vue</span>
+        <span>Pick'em</span>
+      </router-link>
+    </v-toolbar-title>
+  </v-app-bar>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+
+    }
+  },
+
+};
+</script>
+<style>
+  a{
+    text-decoration: none;
+  }
+  a span{
+    text-decoration: none;
+    color:#6D8794;
+  }
+</style>
