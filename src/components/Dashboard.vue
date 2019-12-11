d31831b47ef52cd7f04e9ac91f0214df555990c5
deficts <diegoar7@live.com>
2019-12-11 17:54:01 -0600

    creando dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+<v-container fluid fill-height>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-img class="white--text align-end" src="@/assets/football.jpg">
+          <v-card-title>Bienvenido a VuePick'em!</v-card-title>
+        </v-img>
+        <v-card-text class="text--primary">
+          <div>Aquí podras hacer cada semana la eleccion de tus equpipos favoritos de la NFL</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="$router.push('/ingreso')" color="blue-grey" class="white--text">
+            <span>Ingresar</span>
+          </v-btn>
+          <v-btn @click="$router.push('/registro')" color="blue-grey" class="white--text">
+            <span>Registrarse</span>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data: () => ({
+
+  }),
+};
+</script>
