54b71494c63926bec559b1dddb517c1cdab831da
deficts <diegoar7@live.com>
2019-12-11 17:54:46 -0600

    modificando archivos iniciales

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="home">
+    <Dashboard/>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Dashboard from '@/components/Dashboard.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Dashboard
   }
 }
 </script>
