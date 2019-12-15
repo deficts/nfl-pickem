c351b7e941fa1ab7065ce395abb79fb834fc7b96
deficts <diegoar7@live.com>
2019-12-15 17:07:01 -0600

    creando view de registro

---
+++ src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid fill-height>
+    <RegisterCard/>
+  </v-container>
+</template>
+
+<script>
+import RegisterCard from '@/components/RegisterCard.vue'
+
+export default {
+  name: 'home',
+  components: {
+    RegisterCard
+  }
+}
+</script>
