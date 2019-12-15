7d558ac83aca1fb165de8d989b2946f09d99959f
deficts <diegoar7@live.com>
2019-12-14 19:03:44 -0600

    creando componente de loggeo

---
+++ src/components/LogCard.vue
@@ -0,0 +1,53 @@
+<template>
+<v-container fluid fill-height>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-form>
+          <v-container>
+            <h1>Login</h1>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="username"
+                  label="Username"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  :type="show1 ? 'text' : 'password'"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <span>¿No tienes cuenta? <router-link to="registro">Registrarse</router-link>
+            </span>
+
+          </v-container>
+        </v-form>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="$router.push('/ingreso')" color="blue-grey" class="white--text">
+            <span>Ingresar</span>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'LogCard',
+  data: () => ({
+
+  }),
+};
+</script>
+
+<style media="screen">
+</style>
