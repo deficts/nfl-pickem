0b78558bc7691bbdbdee8c457e86e4616ac9be92
deficts <diegoar7@live.com>
2019-12-14 19:03:32 -0600

    creando vista de loggeo

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid fill-height>
+    <LogCard/>
+  </v-container>
+</template>
+
+<script>
+import LogCard from '@/components/LogCard.vue'
+
+export default {
+  name: 'home',
+  components: {
+    LogCard
+  }
+}
+</script>
